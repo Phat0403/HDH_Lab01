a1e1ce8f24efd9bd28c697298aec6692e71fb39c
Phat0403 <dangtanphat9a1@gmail.com>
2024-10-24 16:39:09 +0700

    add pingpong.c and primes.c - 22120261

---
+++ user/pingpong.c
@@ -0,0 +1,23 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+void main(){
+    int p[2];  
+    pipe(p);   
+
+    if (fork() == 0) {  
+        char buf[1];
+        read(p[0], buf, 1);  
+        printf("%d: received ping\n", getpid());  
+        write(p[1], buf, 1); 
+        exit(0);
+    } else {  
+        char buf[1] = {'a'};
+        write(p[1], buf, 1);  
+        wait(0);             
+        read(p[0], buf, 1);  
+        printf("%d: received pong\n", getpid());  
+        exit(0);
+    }
+}
